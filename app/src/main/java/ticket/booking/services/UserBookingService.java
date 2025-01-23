77977599161e153af5c903f1975c4887b6fd90ab
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-23 22:10:12 +0530

    Add JSON data for trains and update booking service with Jackson integration

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,11 +1,21 @@
 package ticket.booking.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+
+import java.io.File;
+import java.util.List;
 
 public class UserBookingService {
     private User user;
+    private List<User> userList;
+    private ObjectMapper object_mapper = new ObjectMapper();
+    private static final String USERS_PATH = "../localDb/users.json";
     public UserBookingService(User user){
         this.user = user;
+        File users = new File(USERS_PATH);
+        userList = object_mapper.readValue(users, new TypeReference<List<User>>() {});
     }
 
 
