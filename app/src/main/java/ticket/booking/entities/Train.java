f890f34add2459cf28bde0cdc96bc69657475e08
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-25 23:35:24 +0530

    Add Train class with constructors, getters, setters, and train info method

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -1,16 +1,65 @@
 package ticket.booking.entities;
 
-import java.sql.Time;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public class Train {
     private String trainId;
     private String trainNo;
-    private Date departureTime;
-    private Date arrivalTime;
     private List<List<Integer>> seats;
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
     private List<String> stations;
+
+    public Train() {
+    }
+
+    public Train(String trainId, String trainNo, List<List<Integer>> seats, Map<String, String> stationTimes,
+            List<String> stations) {
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations = stations;
+    }
+
+    public List<String> getStations() {
+        return stations;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public Map<String, String> getStationTimes() {
+        return stationTimes;
+    }
+
+    public String getTrainNo() {
+        return trainNo;
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainNo(String trainNo) {
+        this.trainNo = trainNo;
+    }
+
+    public void setStationTimes(Map<String, String> stationTimes) {
+        this.stationTimes = stationTimes;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public void setStations(List<String> stations) {
+        this.stations = stations;
+    }
+
+    public String getTrainInfo() {
+        return String.format("Train Id: %s Train No: %s", trainId, trainNo);
+    }
+
 }
