65b53ccf13a0c1fac010e6b321644ab653d4f610
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-25 22:39:29 +0530

    Refactor UserBookingService to update user data path and add loginUser method for user authentication

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -5,18 +5,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
     private List<User> userList;
     private ObjectMapper object_mapper = new ObjectMapper();
-    private static final String USERS_PATH = "../localDb/users.json";
-    public UserBookingService(User user){
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
         this.user = user;
         File users = new File(USERS_PATH);
-        userList = object_mapper.readValue(users, new TypeReference<List<User>>() {});
+        userList = object_mapper.readValue(users, new TypeReference<List<User>>() {
+        });
     }
 
-
+    public boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user->{
+            return user.getName().equals(.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
 }
