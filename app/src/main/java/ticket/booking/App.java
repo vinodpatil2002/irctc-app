3da127c672a7fa147e85b65fdb166610399d4b78
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-26 14:09:20 +0530

    Implement user signup and login functionality in the main application

--- app/src/main/java/ticket/booking/App.java
+++ app/src/main/java/ticket/booking/App.java
@@ -3,8 +3,60 @@
  */
 package ticket.booking;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.UUID;
+
+import ticket.booking.entities.User;
+import ticket.booking.services.UserBookingService;
+import ticket.booking.utils.UserServiceUtil;
+
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Welcome To Train Booking System: ");
+        Scanner s = new Scanner(System.in);
+        int option = 0;
+        UserBookingService userBookingService;
+        try {
+            userBookingService = new UserBookingService(null)
+        } catch (Exception e) {
+            System.err.println("There is something wrong");
+            return;
+        }
+        while (option != 7) {
+            System.out.println("Choose an option");
+            System.out.println("1. Sign Up");
+            System.out.println("2. Login");
+            System.out.println("3. Fetch Booking");
+            System.out.println("4. Search Trains");
+            System.out.println("5. Book a seat");
+            System.out.println("6. Cancel My Booking");
+            System.out.println("7. Exit the App");
+            option = s.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Enter Username to signup");
+                    String nameToSignUp = s.next();
+                    System.out.println("Enter password");
+                    String passwordToSignUp = s.next();
+                    User userToSignUp = new User(nameToSignUp,passwordToSignUp,UserServiceUtil.hashPassword(passwordToSignUp),new ArrayList<>(),UUID.randomUUID().toString());
+                    userBookingService.signUp(userToSignUp);
+                    break;
+                case 2:
+                    System.out.println("Enter username to login");
+                    String usernameToLogin = s.next();
+                    System.out.println("Enter your password");
+                    String passwordToLogin = s.next();
+                    User userToLogin = new User(usernameToLogin,passwordToLogin,UserServiceUtil.hashPassword(passwordToLogin),new ArrayList<>(),UUID.randomUUID().toString());
+                    try {
+                        userBookingService = new UserBookingService(userToLogin);
+                    } catch (Exception e) {
+                        System.err.println("Maybe there are some error bruh!");
+                        return;
+                    }
+                default:
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -9,17 +9,26 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class UserBookingService {
     private User user;
     private List<User> userList;
     private ObjectMapper object_mapper = new ObjectMapper();
-    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
+    private final String USERS_PATH = "app/src/main/java/ticket/booking/localDb/users.json";
 
     public UserBookingService(User user) throws IOException {
         this.user = user;
+        loadUsers();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUsers();
+    }
+
+    public List<User> loadUsers() throws IOException {
         File users = new File(USERS_PATH);
-        userList = object_mapper.readValue(users, new TypeReference<List<User>>() {
+        return userList = object_mapper.readValue(users, new TypeReference<List<User>>() {
         });
     }
 
@@ -44,6 +53,28 @@ public class UserBookingService {
     public void saveUserListToFile() throws IOException {
         File usersFile = new File(USERS_PATH);
         object_mapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBooking() {
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your ticket Id to cancel");
+        ticketId = s.next();
+        if (ticketId == null || ticketId.isEmpty()) {
+            System.out.println("TicketId cannot be empty");
+            return Boolean.FALSE;
+        }
+        String finalTicketId = ticketId;
+        boolean removed = user.getTicketsBooked().removeIf(ticket -> ticket.getTicketId().equals(finalTicketId));
+        if (removed) {
+            return Boolean.TRUE;
+        } else {
+            System.out.println("No ticket found with id: " + ticketId);
+            return Boolean.FALSE;
+        }
     }
 
 }
