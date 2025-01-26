6b1e3d1466d4a7385521fcac9b60b14df6fe1376
vinodpatil2002 <vinodnspatil@gmail.com>
2025-01-26 11:11:20 +0530

    Add Ticket class with constructors, getters, setters, and ticket info method

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -7,6 +7,71 @@ public class Ticket {
     private String userId;
     private String source;
     private String destination;
-    private Date dateOfTravel;
+    private String dateOfTravel;
     private Train train;
+
+    public Ticket() {
+    };
+
+    public Ticket(String ticketId, String userId, String source, String destination, String dateOfTravel, Train train) {
+        this.ticketId = ticketId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getTicketInfo() {
+        return String.format("Ticket Id: %s belongs to User %s from %s to %s on %s", ticketId, userId, source,
+                destination, dateOfTravel);
+    }
 }
